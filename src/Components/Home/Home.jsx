5da0d2dc4f4aa4f4ecf53d54288f7523df55e68f
kashyap1010 <golakiyakashyap1@gmail.com>
2022-12-08 05:28:12 -0500

    final coomit

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,7 +1,7 @@
 import { Box } from '@mui/system'
 import React from 'react'
 import Navbar from '../Navbar'
-import Menu from './Menu'
+// import Menu from './Menu'
 import Category from './Category'
 
 
